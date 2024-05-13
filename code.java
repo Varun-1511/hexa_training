74cede9f0a6bb58f9bcb53a0f4efdf1058d0ee22
Varun-1511 <134585263+Varun-1511@users.noreply.github.com>
2024-05-13 15:36:24 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,13 @@
+class hello->//class name  {
+    public static void main(String args[])
+    {
+        System.out.print("Hello Wolrd!"); //used to print 
+    }
+}
+
+print()
+
+object, class 
+
+first.. executes main function
+main function contains -> sub fuction()
